8647395822b96790d1a8a1a2d76bc3368ea8039c
otofu-square <yukita@feedforce.jp>
2017-11-12 16:42:52 +0900

    :wrench: Add build settings

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: __dirname + '/build',
+    filename: 'index.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  devtool: 'inline-source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin()],
+};
